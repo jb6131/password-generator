7d7c32430ad03cf26b033c3c329d128432bca193
jb6131 <44068903+jb6131@users.noreply.github.com>
2023-07-04 15:57:17 -0500

    added first prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,19 @@
 // Assignment Code
+
+
+function generatePassword() {
+  var passwordLength = window.prompt("How long would you like the password? Enter a number between 8 and 128.");
+
+  if (!passwordLength) {
+    return;
+  }
+}
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
